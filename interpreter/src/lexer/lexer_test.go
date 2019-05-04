400c06c79a3e66f93d8385e4f626fb01783e2734
theikeofficial <ajzenhamernikola@gmail.com>
2019-05-04 18:19:18 +0200

    Dodati ostali operatori i jos kljucnih reci

--- interpreter/src/lexer/lexer_test.go
+++ interpreter/src/lexer/lexer_test.go
@@ -9,12 +9,23 @@ func TestNextToken(t *testing.T)  {
 	input := `
 		let five = 5;
 		let ten = 10;
-		
+
 		let add = fn(x, y) {
 			x + y;
 		};
 
 		let result = add(five, ten);
+
+		-!*/<>
+
+		if (5 < 10) {
+			return true;
+		} else {
+			return false;
+		}
+
+		125 == 125;
+		125 != 125;
 	`
 
 	tests := []struct{
@@ -56,6 +67,37 @@ func TestNextToken(t *testing.T)  {
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.MINUS, "-"},
+		{token.BANG, "!"},
+		{token.ASTERISK, "*"},
+		{token.SLASH, "/"},
+		{token.LT, "<"},
+		{token.GT, ">"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.INT, "125"},
+		{token.EQ, "=="},
+		{token.INT, "125"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "125"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "125"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
