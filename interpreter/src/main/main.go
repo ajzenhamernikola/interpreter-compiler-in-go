e341a6be9a774c5109eeb7480d20fdd0ce27f608
theikeofficial <ajzenhamernikola@gmail.com>
2019-05-04 18:29:36 +0200

    Zapoceta implementacija REPL-a. Za sada se vrsi prepoznavanje tokena.

---
+++ interpreter/src/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	user2 "os/user"
+	"repl"
+)
+
+func main()  {
+	user, err := user2.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Zdravo %s! Ovo je programski jezik Monkey!\n", user.Username)
+	fmt.Printf("Otvoren je REPL za izvrsavanje komandi\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
