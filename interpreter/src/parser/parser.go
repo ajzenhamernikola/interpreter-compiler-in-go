18c2e5144c514a1cf4bae3ab6ee20fdb963e40e0
theikeofficial <ajzenhamernikola@gmail.com>
2019-05-05 19:26:53 +0200

    Zapoceto kreiranje AST i parsera

---
+++ interpreter/src/parser/parser.go
@@ -0,0 +1,34 @@
+package parser
+
+import (
+	"ast"
+	"lexer"
+	"token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currToken	token.Token
+	peekToken	token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens,
+	// so currToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
